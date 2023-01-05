e7136e12fb7b7ab6aa1d454bafa313127f33088a
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-01-05 22:50:35 +0500

    App Added

---
+++ android/app/src/main/kotlin/com/example/tea_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tea_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,72 @@
+import 'package:book_app/data.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //app bar
+    final appBar = AppBar(
+      elevation: .5,
+      backgroundColor: Colors.black54,
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: () {},
+      ),
+      title: Text('Books App'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {},
+        )
+      ],
+    );
+
+    ///create book tile hero
+    createTile(Book book) => Hero(
+          tag: book.title,
+          child: Material(
+            elevation: 15.0,
+            shadowColor: Colors.orange,
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, 'detail/${book.title}');
+              },
+              child: Image(
+                image: AssetImage(book.image),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        );
+
+    ///create book grid tiles
+    final grid = CustomScrollView(
+      primary: false,
+      slivers: <Widget>[
+        SliverPadding(
+          padding: EdgeInsets.all(16.0),
+          sliver: SliverGrid.count(
+            childAspectRatio: 2 / 3,
+            crossAxisCount: 3,
+            mainAxisSpacing: 20.0,
+            crossAxisSpacing: 20.0,
+            children: books.map((book) => createTile(book)).toList(),
+          ),
+        )
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: appBar,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ""),
+        ],
+      ),
+      body: grid,
+    );
+  }
+}
